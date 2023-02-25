7f78bdbc107e15ee6f56f4e66144a103d5d7ef54
Ravichand K <ravichand.kowtharapu@campsystems.com>
2023-02-25 21:53:57 +0530

    Setting excel style

--- main/Console/Program.cs
+++ main/Console/Program.cs
@@ -10,18 +10,18 @@ class TestClass
     {
         //For testing purpose, the argument has been set to "Test".
         //The program will use the HotelRates.json in the project
-        if(args.Length == 1 && args[0] == "Test")
+        if (args.Length == 1 && args[0] == "Test")
         {
             args[0] = $"{AppDomain.CurrentDomain.BaseDirectory}/HotelRates.json";
         }
 
-        if(args.Length <= 0)
+        if (args.Length <= 0)
         {
             Console.WriteLine("File Name should be mentioned as the first argument");
             return;
         }
 
-        if(!File.Exists(args[0]))
+        if (!File.Exists(args[0]))
         {
             Console.WriteLine("The first argument should be json file name");
             return;
@@ -32,20 +32,29 @@ class TestClass
         new ConfigureServices().Configure(services);
         new ConfigureRepositories().Configure(services);
 
+        string excelGenerated;
         using (var provider = services.BuildServiceProvider())
         {
             var hotelRatesService = provider.GetService<IHotelRatesService>();
 
             using (var stream = new FileInfo(args[0]).OpenRead())
             {
-                var excelGenerated = hotelRatesService.GenerateExcel(stream);
+                excelGenerated = hotelRatesService.GenerateExcel(stream);
 
                 Process.Start("explorer", excelGenerated);
             }
         }
+
+        CleanOldFiles(excelGenerated);
+    }
+
+    private static void CleanOldFiles(string excelGenerated)
+    {
+        Directory
+                    .GetFiles(Path.GetDirectoryName(excelGenerated)).ToList()
+                    .Except(new[] { excelGenerated }).ToList()
+                    .ForEach(file => File.Delete(file));
     }
 }
-
-
 
 
--- main/Repositories/HotelRatesExcelRepository.cs
+++ main/Repositories/HotelRatesExcelRepository.cs
@@ -1,6 +1,7 @@
 ﻿using HotelRates.Excel.Models;
 using OfficeOpenXml;
 using OfficeOpenXml.Style;
+using System.Drawing;
 
 namespace HotelRates.Excel.Repositories
 {
@@ -15,10 +16,7 @@ namespace HotelRates.Excel.Repositories
         {
             if (hotelRates == null || hotelRates.Count <= 0) return null;
 
-            if (!Directory.Exists(@"C:\local")) Directory.CreateDirectory(@"C:\local");
-            if (!Directory.Exists(@"C:\local\HotelRatesExcels")) Directory.CreateDirectory(@"C:\local\HotelRatesExcels");
-
-            var fileName = $@"C:\local\HotelRatesExcels\output_{DateTime.UtcNow.Ticks}.xlsx";
+            var fileName = GenerateFileName();
 
             ExcelPackage.LicenseContext = LicenseContext.NonCommercial;
 
@@ -26,65 +24,103 @@ namespace HotelRates.Excel.Repositories
             {
                 var workSheet = excel.Workbook.Worksheets.Add("Sheet1");
 
-                // setting the properties
-                // of the work sheet 
-                workSheet.TabColor = System.Drawing.Color.Black;
-                workSheet.DefaultRowHeight = 12;
-
-                // Setting the properties
-                // of the first row
-                workSheet.Row(1).Height = 20;
-                workSheet.Row(1).Style.HorizontalAlignment = ExcelHorizontalAlignment.Center;
-                workSheet.Row(1).Style.Font.Bold = true;
-
-                // Header of the Excel sheet
-                workSheet.Cells[1, 1].Value = "ARRIVAL_DATE";
-                workSheet.Cells[1, 2].Value = "DEPARTURE_DATE";
-                workSheet.Cells[1, 3].Value = "PRICE";
-                workSheet.Cells[1, 4].Value = "CURRENCY";
-                workSheet.Cells[1, 5].Value = "RATENAME";
-                workSheet.Cells[1, 6].Value = "ADULTS";
-                workSheet.Cells[1, 7].Value = "BREAKFAST_INCLUDED";
-
-                // Inserting the article data into excel
-                // sheet by using the for each loop
-                // As we have values to the first row 
-                // we will start with second row
-                int recordIndex = 2;
-
-                foreach (var hotelRate in hotelRates)
-                {
-                    workSheet.Cells[recordIndex, 1].Value = hotelRate.ArrivalDate;
-                    workSheet.Cells[recordIndex, 1].Style.Numberformat.Format = "yy.mm.dd";
-                    workSheet.Cells[recordIndex, 2].Value = hotelRate.DepartureDate;
-                    workSheet.Cells[recordIndex, 2].Style.Numberformat.Format = "yy.mm.dd";
-                    workSheet.Cells[recordIndex, 3].Value = hotelRate.Price;
-                    workSheet.Cells[recordIndex, 3].Style.Numberformat.Format = "0.00";
-                    workSheet.Cells[recordIndex, 4].Value = hotelRate.Currency;
-                    workSheet.Cells[recordIndex, 5].Value = hotelRate.RateName;
-                    workSheet.Cells[recordIndex, 6].Value = hotelRate.Adults;
-                    workSheet.Cells[recordIndex, 7].Value = hotelRate.BreakFastIncluded ? 1 : 0;
-
-                    recordIndex++;
-                }
+                SetHeaderStyle(workSheet);
+                AddHeader(workSheet);
+                AddRows(hotelRates, workSheet);
 
                 // By default, the column width is not set to auto fit for the content
                 // of the range, so we are usingAutoFit() method here. 
                 for (var i = 1; i <= 7; i++)
                     workSheet.Column(i).AutoFit();
 
-                if (File.Exists(fileName))
-                    File.Delete(fileName);
-
-                // Create excel file on physical disk 
-                FileStream objFileStrm = File.Create(fileName);
-                objFileStrm.Close();
-
-                // Write content to excel file 
-                File.WriteAllBytes(fileName, excel.GetAsByteArray());
+                WriteToFile(fileName, excel);
             }
 
             return fileName;
+        }
+
+        private static string GenerateFileName()
+        {
+            if (!Directory.Exists(@"C:\local")) Directory.CreateDirectory(@"C:\local");
+            if (!Directory.Exists(@"C:\local\HotelRatesExcels")) Directory.CreateDirectory(@"C:\local\HotelRatesExcels");
+
+            var fileName = $@"C:\local\HotelRatesExcels\output_{DateTime.UtcNow.Ticks}.xlsx";
+            return fileName;
+        }
+
+        private static void AddRows(IList<HotelRate> hotelRates, ExcelWorksheet workSheet)
+        {
+            // Inserting the article data into excel sheet by using the for each loop
+            // As we have values to the first row we will start with second row
+            int recordIndex = 2;
+
+            foreach (var hotelRate in hotelRates)
+            {
+                workSheet.Cells[recordIndex, 1].Value = hotelRate.ArrivalDate;
+                workSheet.Cells[recordIndex, 1].Style.Numberformat.Format = "yy.mm.dd";
+                workSheet.Cells[recordIndex, 2].Value = hotelRate.DepartureDate;
+                workSheet.Cells[recordIndex, 2].Style.Numberformat.Format = "yy.mm.dd";
+                workSheet.Cells[recordIndex, 3].Value = hotelRate.Price;
+                workSheet.Cells[recordIndex, 3].Style.Numberformat.Format = "0.00";
+                workSheet.Cells[recordIndex, 4].Value = hotelRate.Currency;
+                workSheet.Cells[recordIndex, 5].Value = hotelRate.RateName;
+                workSheet.Cells[recordIndex, 6].Value = hotelRate.Adults;
+                workSheet.Cells[recordIndex, 7].Value = hotelRate.BreakFastIncluded ? 1 : 0;
+
+                for (var i = 1; i <= 7; i++)
+                {
+                    workSheet.Cells[recordIndex, i].Style.Font.Color.SetColor(0, 68, 114, 196);
+                    workSheet.Cells[recordIndex, i].Style.Font.Size = 12;
+                    if (recordIndex % 2 == 0)
+                    {
+                        workSheet.Cells[recordIndex, i].Style.Fill.PatternType = ExcelFillStyle.Solid;
+                        workSheet.Cells[recordIndex, i].Style.Fill.BackgroundColor.SetColor(0, 217, 225, 242);
+                    }
+                }
+
+                recordIndex++;
+            }
+        }
+
+        private static void WriteToFile(string fileName, ExcelPackage excel)
+        {
+            if (File.Exists(fileName))
+                File.Delete(fileName);
+
+            // Create excel file on physical disk 
+            FileStream objFileStrm = File.Create(fileName);
+            objFileStrm.Close();
+
+            // Write content to excel file 
+            File.WriteAllBytes(fileName, excel.GetAsByteArray());
+        }
+
+        private static void SetHeaderStyle(ExcelWorksheet workSheet)
+        {
+            // setting the properties
+            // of the work sheet 
+            workSheet.TabColor = System.Drawing.Color.Black;
+            workSheet.DefaultRowHeight = 12;
+
+            // Setting the properties
+            // of the first row
+            workSheet.Row(1).Height = 20;
+            workSheet.Row(1).Style.HorizontalAlignment = ExcelHorizontalAlignment.Center;
+            workSheet.Row(1).Style.Font.Bold = true;
+            workSheet.Row(1).Style.Font.Color.SetColor(0, 68, 114, 196);
+            workSheet.Row(1).Style.Font.Size = 12;
+        }
+
+        private static void AddHeader(ExcelWorksheet workSheet)
+        {
+            // Header of the Excel sheet
+            workSheet.Cells[1, 1].Value = "ARRIVAL_DATE";
+            workSheet.Cells[1, 2].Value = "DEPARTURE_DATE";
+            workSheet.Cells[1, 3].Value = "PRICE";
+            workSheet.Cells[1, 4].Value = "CURRENCY";
+            workSheet.Cells[1, 5].Value = "RATENAME";
+            workSheet.Cells[1, 6].Value = "ADULTS";
+            workSheet.Cells[1, 7].Value = "BREAKFAST_INCLUDED";
         }
     }
 }
